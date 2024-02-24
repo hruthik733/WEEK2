d665557be0e22ee57b1fc7d3a7edea75e2da2db4
hruthik <hruthik733@gmail.com>
2024-02-24 20:51:31 +0530

    working on CSV file using R

--- CSV.R
+++ CSV.R
@@ -2,15 +2,16 @@
 >   View(csv.dataset)
 > head(csv.dataset)
 Emp_Id satisfaction_level last_evaluation
-1 IND02438                38%             53%
+  1 IND02438                38%             53%
   2 IND28133                80%             86%
   3 IND07164                11%             88%
   4 IND30478                72%             87%
   5 IND24003                37%             52%
   6 IND08609                41%             50%
-  > tail(csv.dataset)
+
+> tail(csv.dataset)
 Emp_Id satisfaction_level last_evaluation
-14 IND20559                41%             55%
+  14 IND20559                41%             55%
   15 IND12896                36%             56%
   16 IND18204                38%             54%
   17 IND43525                45%             47%
