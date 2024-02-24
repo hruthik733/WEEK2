5ad7403024b58ab8d55b9e608b8c4f133d1519e2
hruthik <hruthik733@gmail.com>
2024-02-24 20:50:16 +0530

    working with CSV file using R

---
+++ CSV.R
@@ -0,0 +1,18 @@
+> csv.dataset <- read.csv("C:/Users/hkp/Downloads/csv-dataset.csv")
+>   View(csv.dataset)
+> head(csv.dataset)
+Emp_Id satisfaction_level last_evaluation
+1 IND02438                38%             53%
+  2 IND28133                80%             86%
+  3 IND07164                11%             88%
+  4 IND30478                72%             87%
+  5 IND24003                37%             52%
+  6 IND08609                41%             50%
+  > tail(csv.dataset)
+Emp_Id satisfaction_level last_evaluation
+14 IND20559                41%             55%
+  15 IND12896                36%             56%
+  16 IND18204                38%             54%
+  17 IND43525                45%             47%
+  18 IND21424                78%             99%
+  19 IND40030                45%             51%
